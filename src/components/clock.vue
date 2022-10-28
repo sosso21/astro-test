eee9c8e08d59d2f022346b716eca4a6bf83f2ce5
sosso21 <sofianetop21.st@gmail.com>
2022-10-28 18:13:07 +0100

    :sparkles: (init): first commit

---
+++ src/components/clock.vue
@@ -0,0 +1,12 @@
+<template>
+<h2> Dynamic date :  {{vueClock}} </h2>
+</template>
+<script>
+export default {
+    setup( ) {
+        return {
+            vueClock: Date()
+        }
+    }
+}
+</script>
